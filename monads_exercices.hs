168b3efbe4c5af64884882a4052840e6a45d2270
raoul1609 <elieraoulnet@gmail.com>
2022-12-07 14:46:55 +0100

    suite exos sur monads : con erroi

--- monads_exercices.hs
+++ monads_exercices.hs
@@ -1,6 +1,14 @@
-type Stack = [Int]
+--import Prelude hiding (Right, Left)
 
--- exercices about monads, cf le livre : Thinking functionally with haskell, de RICHARD BIRD --
+module MyMonad where 
+
+
+
+import qualified Control.Monad as C
+import qualified Control.Applicative as A
+import Prelude hiding (fmap, Left, Right)
+
+{--== exercices about monads, cf le livre : Thinking functionally with haskell, de RICHARD BIRD ==--}
 
 -- fonction qui additionne 3 nombres de types Maybe Int : version simple  
 
@@ -17,5 +25,54 @@ addMonad x y z = do
     i <- x 
     j <- y 
     k <- z 
-    return (i + j + k)
+    C.return (i + j + k)
 
+
+
+{-== others exercices about monads, cf le livre : haskell programming from first principles ==-}
+
+-- 1 
+data Nope a = NopeDotJpg   -- c'est un type constant, non è difficile 
+
+instance Functor Nope where 
+    fmap f x = NopeDotJpg
+instance Applicative Nope where 
+    pure x = NopeDotJpg
+    f <*> x = NopeDotJpg
+instance Monad Nope where 
+    return = A.pure 
+    x >>= f = NopeDotJpg
+
+--2
+data PhhhbbtttEither b a = Left a | Right b --molto importante: il primo parametro fissato nel costruttore del tipo :: b, non è che le premier parametre doit toujours:: a, dipende della posizione 
+
+instance Functor (PhhhbbtttEither r) where 
+    fmap f (Left x) = Left (f x)
+    fmap f (Right y) = Right y 
+
+instance Applicative (PhhhbbtttEither e) where -- la fonction ne peut etre contenue que dans le Left car le Right a un parametre deja fixé, y :: PhhhbbtttEither b a quindi c'est mieux d'utiliser 
+                                                -- la fmap definie dans l'instance de Functor que d'utiliser le pattern matching 
+    pure x = Left x 
+    Left f <*> y = fmap f y   -- je ne peux pas me limiter a definir seulement Left, si il y a Left il faut un Right   
+    Right x <*> _ = Right x  
+
+instance Monad (PhhhbbtttEither e) where 
+    return = MyMonad.pure 
+    Right x >>= _ = Right x 
+    Left y >>= f = f y  -- f met dans le contexte, donc inutile d'ecrire : Left (f y)
+
+--3
+newtype Identity a = Identity a deriving (Eq, Ord, Show)
+
+instance Functor Identity where
+fmap f x = Identity (f x)
+
+instance Applicative Identity where
+pure  = Identity 
+(<*>) (Identity f) (Identity s) = Identity (f s)
+
+instance Monad Identity where
+return = pure
+(>>=) (Identity a) f = f a 
+    
+     
