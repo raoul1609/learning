65d6b1d16f0f4f0c5d0fe0ff9412a22fa2c6d128
Nkalla <elieraoulnet@gmail.com>
2023-12-03 12:06:37 +0100

    continuos learning, le savoir se trouve au bout de l'effort

---
+++ Py_Ts_Js_learning/apprendimentoPython.py
@@ -0,0 +1,9 @@
+# voici l'exemple d'un commentaire en python
+# apprendimento per quanto riguarda la documentazione officiale di python : molto interresante et rica
+# NB : la division simple (/) en pyhton donne toujours un float
+# la division entiere : // , le reste de la division entiere : % 
+# 3 * 'raoul' : repete la chaine 'raoul' 3 fois
+# deux chaines cote a cote sont automatiquement concatenés : 'raoul' 'bonjour' => 'raoulbonjour'.
+# les chaines de caracteres sont immuables ou immutables en python, car affecter une nouvelle valeur a un indice d'une chaine produit une erreur.
+
+# je me suis arreté sur le troisieme point, prochaine leture je commence sur :  4.D'autres outils de contrôle de flux
