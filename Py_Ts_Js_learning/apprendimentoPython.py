65b95cb0c32be0ca1c362cb7d11307f671a0545a
Neudjieu Raoul <elieraoulnet@gmail.com>
2024-10-06 01:00:29 +0200

    continuos with lettura official python's doc, 06/10/2024

--- Py_Ts_Js_learning/apprendimentoPython.py
+++ Py_Ts_Js_learning/apprendimentoPython.py
@@ -5,5 +5,30 @@
 # 3 * 'raoul' : repete la chaine 'raoul' 3 fois
 # deux chaines cote a cote sont automatiquement concatenés : 'raoul' 'bonjour' => 'raoulbonjour'.
 # les chaines de caracteres sont immuables ou immutables en python, car affecter une nouvelle valeur a un indice d'une chaine produit une erreur.
-
 # je me suis arreté sur le troisieme point, prochaine leture je commence sur :  4.D'autres outils de contrôle de flux
+
+
+
+# jeudi, 26 septembre 2024 : reprise avec l'apprentissage de python, lecture de la documentation officielle 
+"""
+- python a un typage dynamique ce qui implique que une variable peut avoir plusieurs types au cours de l'execution d'un script python
+
+- on peut ecrire la boucle if sans le else 
+- on peut ecrire la boucle if avec beaucoup de elif
+
+- la boucle for a son else qui est exécuté une fois que la boucle atteint son itération finale.
+
+- la boucle while a son else aussi, qui est executé après que la condition de la boucle devient fausse.
+NB : Dans l’un ou l’autre type de boucle, la elseclause n’est pas exécutée si la boucle a été terminée par un break.
+
+- l'instruction continue : permet de continuer avec l'iteration suivante
+- l'instruction pass : ne fait rien, Elle peut être utilisée lorsqu'une instruction est requise syntaxiquement 
+    mais que le programme ne nécessite aucune action
+le pass peut aussi etre utilisé comme le undefined dans haskell, lorsqu'on est en plein reflexion sur un code 
+
+- l'instruction match permet de comparer la valeur d'une expression à des modèles successifs, 
+        donnés sous la forme d'un ou plusieurs blocs case
+- dans une instruction il y a le cas terminal : le _ qui agit comme le otherwise de haskell
+NB: l'intruction match est similaire au pattern matching dans haskell
+
+"""
