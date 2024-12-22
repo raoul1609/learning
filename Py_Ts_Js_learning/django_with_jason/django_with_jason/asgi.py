e412d1aceb413be73395c0fb1eb9e92714de8ae7
raoul1609 <elieraoulnet@gmail.com>
2024-12-22 16:20:56 +0100

    get started with django following jason champagne tuto on youtube, 22/12/2024

--- Py_Ts_Js_learning/django_with_jason/django_with_jason/asgi.py
+++ Py_Ts_Js_learning/django_with_jason/django_with_jason/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for neudjieu_site project.
+ASGI config for django_with_jason project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'neudjieu_site.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_with_jason.settings')
 
 application = get_asgi_application()
--- Py_Ts_Js_learning/django_with_jason/manage.py
+++ Py_Ts_Js_learning/django_with_jason/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'neudjieu_site.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_with_jason.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
