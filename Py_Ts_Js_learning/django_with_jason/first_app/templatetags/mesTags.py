d4fe4cc1f74e28581394a03af3e9175b5d5a3631
raoul1609 <elieraoulnet@gmail.com>
2025-01-15 17:54:36 +0100

    balise et filtre, j'ai cree et teste mes propres filtres c'est ok, j'ai pas experimente les balises

--- Py_Ts_Js_learning/django_with_jason/first_app/templatetags/mesTags.py
+++ Py_Ts_Js_learning/django_with_jason/first_app/templatetags/mesTags.py
@@ -2,10 +2,15 @@ from django import template
 from django.template.defaultfilters import stringfilter
 
 #comment creer ses propres filtres avec django
+#chaque filtre commence avec la ligne du decorateur
 register = template.Library()
 
 @register.filter
 @stringfilter
-
 def monFiltre (value) :
     return value.upper()
+
+# pour appeler le filtre dans le template, j'utilise son nom au lieu du nom de la fonction
+@register.filter (name= "le nom de mon autre filtre")
+def autreFiltre (value, uneTaille):
+    return len (value) == uneTaille
